42c54d6a5c318cea6cc05a7e37364ac9c503768c
Varga Donat <vdonatj2@gmail.com>
2021-08-13 19:30:32 +0200

    Updated sites.html </body></html>+nav.js

--- newSite/nav.js
+++ newSite/nav.js
@@ -1,3 +1,1 @@
-document.write(`<nav><a href=index.html>Home</a>
-<a href=sites.html>My GitHub sites</a>
-<a href=repositories.html>My GitHub repositories</a></nav>`)
+document.write('<nav><a href=index.html>Home</a><a href=sites.html>My GitHub sites</a></nav>')
