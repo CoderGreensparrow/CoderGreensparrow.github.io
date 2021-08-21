9bd6dbcacf84d12d94772f1b519b9fda5a676e91
Varga Donat <vdonatj2@gmail.com>
2021-08-21 19:47:04 +0200

    CHANGED NEWS

--- news.js
+++ news.js
@@ -1,3 +1,3 @@
-news=['The OLNMLN interpreter site is online!','/OLNMLN']
+news=['The OLNMLN JavaScript interpreter has been updated!','/OLNMLN']
 document.write(`<div id=news>${news[0]} <a href=${news[1]}>Check it out</a><div id=exit onclick=closeNews()>X</div></div>`)
 function closeNews(){document.getElementById('news').style.display='none'}
