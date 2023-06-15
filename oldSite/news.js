0d00dd85301ed064e6b8d88d3c012ed9a9eec340
VargaDonát <vdonatj2@gmail.com>
2023-06-15 16:34:14 +0200

    Site update

--- oldSite/news.js
+++ oldSite/news.js
@@ -1,3 +1,3 @@
-news=['The OLNMLN JavaScript interpreter has been updated!','/OLNMLN']
+news=['Games to play on your calculator!','/calcgames.html']
 document.write(`<div id=news>${news[0]} <a href=${news[1]}>Check it out</a><div id=exit onclick=closeNews()>X</div></div>`)
 function closeNews(){document.getElementById('news').style.display='none'}
